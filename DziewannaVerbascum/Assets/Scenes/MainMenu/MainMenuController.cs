838ce2c93ab5a83ac744a3c93217d4cf8a12a5e1
lukesawicki <lukaszsa1@gmail.com>
2021-01-30 17:40:11 +0100

    Swtiching between menus
    Swtiching between menus

---
+++ DziewannaVerbascum/Assets/Scenes/MainMenu/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void play()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene("VerbascumScene", LoadSceneMode.Single);
+    }
+
+    public void quit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
